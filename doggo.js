298c2fa7791b5109c26c8c678d0ab2a5ec517b1c
Guillermo Rodriguez <guillermor@mail.usf.edu>
2020-04-20 15:09:45 -0600

    Basic functionality, no responsive images yet

--- doggo.js
+++ doggo.js
@@ -3,85 +3,38 @@ const DOG_URL = "https://dog.ceo/api/breeds/image/random"; // DOG CEO Random API
 const cards = document.querySelector(".cards"); // Cards Section to append cards to
 const cardCollection = cards.children; // The children of the cards (imgs)
 const inputDoggos = document.querySelector("input"); // Input Slider
-const playGame = document.querySelector(".button--play-game"); // Play-Game Button
-const startOver = document.querySelector(".button--start-over"); // Start-Over Button
+const playGameButton = document.querySelector(".button--play-game"); // Play-Game Button
 
-// Internal Array of Doggos
-let allTheCards = []; // [ [1, 2], [ 3,4 ], [ 5,6 ] ]
+let doggoImages = ["./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg"];
 
-// 1 PLAY GAME BUTTON!
-playGame.addEventListener("click", function () {
-  // First delete cards
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+// PLAY GAME!
+playGameButton.addEventListener("click", function () {
+  // 1 FIRST DELETE CARDS
   deleteCards();
-  createCards(inputDoggos.value); // Create the amount of cards given by the value of the slider
+
+  // 2 CALL THE API
+  //getDoggos(inputDoggos.value);
+
+  // 3 CREATE THE CARDS INPUT
+  //createCards(inputDoggos.value);
+  createCards(8); //TESTING
+
+  // 4 GIVE CARDS THE DOGGO IMAGES
+  renderCards();
+
+  // 5 SHUFFLE CARDS
+  shuffleCards(8);
 });
 
-// 2 START OVER BUTTON!
-startOver.addEventListener("click", function () {
-  //Just delete the cards
-  deleteCards();
-});
-
-// This will create the amount of Doggo Cards based on input passed in
-function createCards(amountOfCards) {
-  let j = 0;
-  let subCardArr = [];
-
-  for (let i = 0; i < amountOfCards; i++) {
-    // Create Card
-    // First new div
-    let newCard = document.createElement("div");
-    // Next the back img
-    let backOfCard = document.createElement("img");
-    let frontOfCard = document.createElement("img")
-    newCard.className = "card";
-    backOfCard.className = "card--back";
-    frontOfCard.className = "card--front";
-    backOfCard.src = "./img/doggo2.jpg";
-    frontOfCard.src = "https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg";
-
-    // INTERNAL: Creates an array of arrays
-    //if (j === 1) {
-      //subCardArr.push(newCard);
-      //// Push to array of Internal doggos
-      //allTheCards.push(subCardArr);
-      //subCardArr = [];
-      //j = 0;
-    //} else {
-      //subCardArr.push(newCard);
-      //j = j + 1;
-    //}
-
-    // Put the back of card into the 
-    newCard.appendChild(backOfCard);
-    newCard.appendChild(frontOfCard);
-
-    // Put the div
-    cards.appendChild(newCard);
-  }
-
-  // Cards must exist to add eventListener to all of them
-  const eachCard = Array.prototype.slice.call(cardCollection); // Convert to Array
-  // Add Flip Card Event Listener to each card
-  eachCard.forEach((card) => card.addEventListener("mousedown", flipCard));
-}
-
-// Delete all the Cards
-function deleteCards() {
-  cards.querySelectorAll("*").forEach((n) => n.remove()); // Select all the cards with .class
-  // Delete the ALL cards Array
-  allTheCards = [];
-}
-
-function flipCard() {
-  // Adds a toggle to the class that transforms the background
-      
-  this.classList.toggle("flip");
-  console.log(this);
-}
-
-// Call APII
 function addNewDoggo() {
+  /**
+   * Calls the dog.ceo API and creates and the doggoImages array
+   * This will become the front of the cards
+   */
   const promise = fetch(DOG_URL);
   promise
     .then(function (response) {
@@ -97,6 +50,125 @@ function addNewDoggo() {
     });
 }
 
-document
-  .querySelector(".button--add-doggo")
-  .addEventListener("click", addNewDoggo);
+function createCards(amountOfCards) {
+  /**
+   * Creates the DIV cards based on slider input
+   * Does not populate the front card with random images
+   */
+  for (let i = 0; i < amountOfCards; i++) {
+    // Create the Div
+    let newCard = document.createElement("div");
+
+    // Append the front/back images
+    let backOfCard = document.createElement("img");
+    let frontOfCard = document.createElement("img");
+
+    newCard.className = "card";
+    backOfCard.className = "card--back";
+    frontOfCard.className = "card--front";
+    backOfCard.src = "./img/doggo2.jpg";
+    frontOfCard.src =
+      "https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg";
+
+    // Append images: Div>BackImg>FrontImg
+    newCard.appendChild(backOfCard);
+    newCard.appendChild(frontOfCard);
+    cards.appendChild(newCard);
+  }
+}
+
+function renderCards() {
+  /**
+   * Populates the front card with random images
+   * Gives each card an event listener
+   */
+  const eachCard = Array.prototype.slice.call(cardCollection); // Convert to Array
+
+  let imageCounter = 0;
+  for (let cardCounter = 0; cardCounter < eachCard.length; cardCounter++) {
+    // If the CardCounter is passed the first 2 AND the last two cards match,
+    // we increment the image Counter
+    // Transforms array [1,2,3,4,5,6,7,8] --> [1,1,2,2,3,3,4,4]
+    if (
+      cardCounter > 1 &&
+      eachCard[cardCounter - 1].children[1].src ==
+        eachCard[cardCounter - 2].children[1].src
+    ) {
+      imageCounter++;
+    }
+
+    // Back Card img
+    eachCard[cardCounter].children[1].src = doggoImages[imageCounter];
+
+    // Add Flip Card Event Listener to each card
+    eachCard[cardCounter].addEventListener("click", flipCard);
+  }
+}
+
+function deleteCards() {
+  /**
+   * Selects all the cards and removes them from the card section
+   */
+  cards.querySelectorAll("*").forEach((n) => n.remove()); // Select all the cards with .class
+}
+
+function flipCard() {
+  /**
+   * Adds a toggle to class to the card  element
+   * That transforms the background from back to front
+   */
+  if (lockBoard) return; // Edgecase lockboard after firstCard click
+  if (this === firstCard) return; // Edgecase secondCard cannot be firstCard too
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // First Click
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  // Second Click
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.children[1].src === secondCard.children[1].src) {
+    // It's a MATCH!
+    disableCards();
+  } else {
+    // NOT a match
+    unflipCards();
+  }
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1420);
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null,  null];
+}
+
+function shuffleCards(numberofCards) {
+  cards.querySelectorAll("*").forEach(card => {
+  let randomPos = Math.floor(Math.random() * numberofCards);
+    card.style.order = randomPos;
+  });
+}
